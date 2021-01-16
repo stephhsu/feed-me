962449cadef9794af0632b787cd7ccfde242b71d
Victoria Ren <vjren@uwaterloo.ca>
2021-01-16 02:20:02 -0500

    setup

--- feed_me/src/App.vue
+++ feed_me/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
---
+++ feed_me/src/components/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hello">
+<h1> Welcome to feed me! </h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+ 
+}
+</script>
