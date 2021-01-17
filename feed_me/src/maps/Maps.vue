1cb1820e375c20298d43a8bb39208175322548c9
jesseren <jesseren80@gmail.com>
2021-01-17 00:21:45 -0500

    maps component initial

--- feed_me/src/maps/Maps.vue
+++ feed_me/src/maps/Maps.vue
@@ -1,10 +1,38 @@
 <template>
-  <div id="map"/>
+  <div class="map"/>
 </template>
 
+<script>
+import gmapsInit from './mapsInit';
+
+export default {
+  name: 'App',
+  async mounted() {
+    try {
+      const google = await gmapsInit();
+      const geocoder = new google.maps.Geocoder();
+      const map = new google.maps.Map(this.$el);
+
+      geocoder.geocode({ address: 'Austria' }, (results, status) => {
+        if (status !== 'OK' || !results[0]) {
+          throw new Error(status);
+        }
+
+        map.setCenter(results[0].geometry.location);
+        map.fitBounds(results[0].geometry.viewport);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+</script>
+
 <style>
-  #map {
-    height: 100%;
-  }
-  
+
+
+.map {
+  width: 300px;
+  height: 300px;
+}
 </style>
