50518d59f9af831155df2c658e2aa87ae7cb183e
jesseren <jesseren80@gmail.com>
2021-01-17 02:31:20 -0500

    fixed map props

--- feed_me/src/maps/Maps.vue
+++ feed_me/src/maps/Maps.vue
@@ -13,29 +13,28 @@ export default {
       required: true,
     },
   },
-  async mounted(props) {
+  async setup(props) {
     try {
       let i
       let south = 0;
       let west = 0;
       let north = 0;
       let east = 0;
-      for (i = 0; i < places.length; i++) {
-          south = south < places[i].geometry.location.lat ?
+      for (i = 0; i < props.places.length; i++) {
+          south = south < props.places[i].geometry.location.lat ?
             south :
-            places[i].geometry.location.lat
-          west = west < places[i].geometry.location.lng ?
+            props.places[i].geometry.location.lat
+          west = west < props.places[i].geometry.location.lng ?
             west :
-            places[i].geometry.location.lng
-          north = north > places[i].geometry.location.lat ?
+            props.places[i].geometry.location.lng
+          north = north > props.places[i].geometry.location.lat ?
             north :
-            places[i].geometry.location.lat
-          east = east > places[i].geometry.location.lng ?
+            props.places[i].geometry.location.lat
+          east = east > props.places[i].geometry.location.lng ?
             east :
-            places[i].geometry.location.lng
+            props.places[i].geometry.location.lng
       }
       const google = await gmapsInit();
-      const geocoder = new google.maps.Geocoder();
       const map = new google.maps.Map(this.$el);
 
       map.setCenter({lat: (north + south) / 2, lng: (east + west) / 2})
